0f151a169e59d55b23756ba237e2912b38c3b6d1
Pranav <code.pranavdp@gmail.com>
2024-10-17 21:59:06 +0530

    added task

--- app.py
+++ app.py
@@ -5,19 +5,22 @@ from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 from functools import wraps
 from pymongo import MongoClient
+from datetime import date
 
 app = Flask(__name__)
 application = app
 
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
+app.secret_key = os.urandom(24)
 Session(app)
 
-MONGODB_URI = "mongodb+srv://todolist:todolist@todolist.0ozmp.mongodb.net/?retryWrites=true&w=majority&appName=ToDoList"
+MONGODB_URI = "mongodb://localhost:27017"
 
 client = MongoClient(MONGODB_URI)
-db = client.todo 
+db = client.Todo 
 users_collection = db.users
+tasks_collection = db.tasks
 
 @app.after_request
 def after_request(response):
@@ -62,19 +65,60 @@ def login():
     session.clear()
 
     if request.method == "POST":
-        user = users_collection.find_one({"username": request.form.get("username")})
+        username = request.form.get("username")
+        password = request.form.get("password")
 
-        if user is None or not check_password_hash(user["hash"], request.form.get("password")):
-            return apology("invalid username and/or password", 403)
+        user = users_collection.find_one({"username": username})
+
+        if user is None:
+            return apology("invalid username")
+        if not check_password_hash(user["hash"], password):
+            return apology("invalid password")
 
         session["user_id"] = str(user["_id"])
 
         return redirect("/")
 
+    return render_template("login.html")
+
+@app.route("/add")
+@login_req
+def add():
+    user_id = session["user_id"]
+    if request.method=="POST":
+        task=request.form.get("task")
+        date=date.today()
+        tasks={"user_id": user_id, "task": task, "date": date}
+        tasks_collection.insert_one(tasks)
+        return redirect("/")
     else:
-        return render_template("login.html")
-    
+        return render_template("add.html")
+
 @app.route("/")
 @login_req
 def index():
     user_id = session["user_id"]
+    tasks=db.tasks.find({"user_id": user_id})
+    len_dates = {}
+    t = {}
+    for row in tasks:
+        task = row["task"]
+        date = row["date"]
+        if date not in len_dates:
+            len_dates[date] = 1
+        else:
+            len_dates[date] += 1
+        if date not in t:
+            t[date] = [task]
+        else:
+            t[date].append([task])
+    return render_template("index.html", t=t, len_dates=len_dates)
+
+@app.route("/logout")
+def logout():
+    """Log user out"""
+    session.clear()
+    return redirect("/")
+
+if __name__ == "__main__":
+    app.run(debug=True)
